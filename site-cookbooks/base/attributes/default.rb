5d6f8dd3e9db88d7bcd01098155e68fa36def72f
blooper05 <legend.of.blooper@gmail.com>
2014-10-06 02:08:59 +0900

    add provisioning of rbenv-binstubs

---
+++ site-cookbooks/base/attributes/default.rb
@@ -0,0 +1,2 @@
+default[:rbenv_binstubs][:git_repository] = "https://github.com/ianheggie/rbenv-binstubs.git"
+default[:rbenv_binstubs][:git_revision]   = "master"
--- site-cookbooks/base/recipes/default.rb
+++ site-cookbooks/base/recipes/default.rb
@@ -18,3 +18,19 @@ versions.each do |version|
     ruby_version version
   end
 end
+
+node.set[:rbenv_binstubs][:prefix] = "#{node[:rbenv][:root]}/plugins/rbenv-binstubs"
+
+include_recipe "git"
+
+with_home_for_user(node[:rbenv][:user]) do
+
+  git node[:rbenv_binstubs][:prefix] do
+    repository node[:rbenv_binstubs][:git_repository]
+    reference node[:rbenv_binstubs][:git_revision]
+    action :sync
+    user node[:rbenv][:user]
+    group node[:rbenv][:group]
+  end
+
+end

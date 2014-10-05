491ef0b0ce68ff30eed73951f267d35f83a06667
blooper05 <legend.of.blooper@gmail.com>
2014-10-06 02:07:24 +0900

    add provisioning of Ruby

--- site-cookbooks/base/recipes/default.rb
+++ site-cookbooks/base/recipes/default.rb
@@ -6,3 +6,15 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+versions = %w(2.1.3)
+
+versions.each do |version|
+  rbenv_ruby version do
+    global true
+  end
+
+  rbenv_gem 'bundler' do
+    ruby_version version
+  end
+end
